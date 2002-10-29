91e0628b54e239b83a71dddf0a751831a5733e1a
hcayless <hcayless@85be4e97-ed4a-0410-b6a7-8d1dfdb05c0b>
2002-10-29 14:26:07 +0000

    Cocoon Transformer that uses the TransCoder classes to perform encoding shifts on XML files.
    git-svn-id: svn+ssh://svn.code.sf.net/p/epidoc/code/trunk@26 85be4e97-ed4a-0410-b6a7-8d1dfdb05c0b

---
+++ transcoder/src/edu/unc/epidoc/cocoon/TranscodingTransformer.java
@@ -0,0 +1,87 @@
+/*
+ * TranscodingTransformer.java
+ *
+ * Created on October 27, 2002, 3:39 PM
+ */
+
+package edu.unc.epidoc.cocoon;
+
+import edu.unc.epidoc.*;
+import java.util.Stack;
+import org.apache.cocoon.transformation.AbstractTransformer;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author  hcayless
+ */
+public class TranscodingTransformer extends AbstractTransformer {
+    
+    public void setup(org.apache.cocoon.environment.SourceResolver sourceResolver, java.util.Map map, String str, org.apache.avalon.framework.parameters.Parameters parameters) throws org.apache.cocoon.ProcessingException, org.xml.sax.SAXException, java.io.IOException {
+        getLogger().debug("TranscodingTransformer setup");
+        tc = new TransCoder();
+        try {
+            tc.setParser(parameters.getParameter("parser", "BetaCode"));
+            tc.setConverter(parameters.getParameter("converter", "Unicode"));
+        } catch (Exception e) {
+            getLogger().error("TranscodingTransformer setup", e);
+        }
+        elements = new Stack();
+    }
+    
+    public void recycle() {
+        language = null;
+        tc = null;
+    }
+        
+    public void startElement(String uri, String name, String raw, org.xml.sax.Attributes attributes)
+    throws SAXException {
+        if (attributes.getValue("lang") != null) {
+            language = attributes.getValue("lang");
+            elements.push(new String[] {name, language});
+        }
+        this.contentHandler.startElement(uri, name, raw, attributes);
+    }
+    
+    public void endElement(String uri, String name, String raw)
+    throws SAXException {
+        if (elements.size() > 0) {
+            String[] elt = (String[])elements.peek();
+            if (elt[0].equals(name)) {
+                elements.pop();
+                if (elements.size() > 0) {
+                    elt = (String[])elements.peek();
+                    language = elt[1];
+                } else
+                    language = "eng";                
+            }
+        }
+        this.contentHandler.endElement(uri, name, raw);
+    }
+        
+    
+    public void characters(char c[], int start, int len)
+    throws SAXException {
+        StringBuffer strb = new StringBuffer();
+        int length = len;
+        strb.append(c);
+        if(tc.getParser().supportsLanguage(language)) {
+            try {
+            String in = new String(c, start, len);
+            String out = tc.getString(in);
+            strb.delete(start, start+len);
+            strb.insert(start, out);
+            length = out.length();            
+            } catch (Exception e) {
+                getLogger().error("TranscodingTransformer characters", e);
+            }
+        }
+        this.contentHandler.characters(strb.toString().toCharArray(), start, length);
+    }
+        
+    
+    private String language = "eng";
+    private TransCoder tc;
+    private Stack elements;
+
+}
