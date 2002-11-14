ea67add14c156f127e20e40f0af19081ea3c7a78
hcayless <hcayless@85be4e97-ed4a-0410-b6a7-8d1dfdb05c0b>
2002-11-14 13:43:45 +0000

    patched bug caused when setup method is called after recycle().
    git-svn-id: svn+ssh://svn.code.sf.net/p/epidoc/code/trunk@49 85be4e97-ed4a-0410-b6a7-8d1dfdb05c0b

--- transcoder/src/edu/unc/epidoc/cocoon/TranscodingTransformer.java
+++ transcoder/src/edu/unc/epidoc/cocoon/TranscodingTransformer.java
@@ -27,38 +27,52 @@ public class TranscodingTransformer extends AbstractTransformer {
             getLogger().error("TranscodingTransformer setup", e);
         }
         elements = new Stack();
+        languages = new Stack();
+        languages.push(parameters.getParameter("language", DEFAULT_LANG));
     }
     
     public void recycle() {
         language = null;
         tc = null;
+        elements = null;
+        languages = null;
     }
-        
+    
     public void startElement(String uri, String name, String raw, org.xml.sax.Attributes attributes)
     throws SAXException {
-        if (attributes.getValue("lang") != null) {
+        if (uri != null && uri.equals(namespace) && name.equals(tcName)) {
             language = attributes.getValue("lang");
-            elements.push(new String[] {name, language});
+            languages.push(attributes.getValue("lang"));
+        } else {
+            if (attributes.getValue("lang") != null) {
+                language = attributes.getValue("lang");
+                elements.push(new String[] {name, language});
+            }
+            this.contentHandler.startElement(uri, name, raw, attributes);
         }
-        this.contentHandler.startElement(uri, name, raw, attributes);
     }
     
     public void endElement(String uri, String name, String raw)
     throws SAXException {
-        if (elements.size() > 0) {
-            String[] elt = (String[])elements.peek();
-            if (elt[0].equals(name)) {
-                elements.pop();
-                if (elements.size() > 0) {
-                    elt = (String[])elements.peek();
-                    language = elt[1];
-                } else
-                    language = "eng";                
+        if (uri != null && uri.equals(namespace) && name.equals(tcName)) {
+            languages.pop();
+            language = (String)languages.peek();
+        } else {
+            if (elements.size() > 0) {
+                String[] elt = (String[])elements.peek();
+                if (elt[0].equals(name)) {
+                    elements.pop();
+                    if (elements.size() > 0) {
+                        elt = (String[])elements.peek();
+                        language = elt[1];
+                    } else
+                        language = "eng";
+                }
             }
+            this.contentHandler.endElement(uri, name, raw);
         }
-        this.contentHandler.endElement(uri, name, raw);
     }
-        
+    
     
     public void characters(char c[], int start, int len)
     throws SAXException {
@@ -67,21 +81,24 @@ public class TranscodingTransformer extends AbstractTransformer {
         strb.append(c);
         if(tc.getParser().supportsLanguage(language)) {
             try {
-            String in = new String(c, start, len);
-            String out = tc.getString(in);
-            strb.delete(start, start+len);
-            strb.insert(start, out);
-            length = out.length();            
+                String in = new String(c, start, len);
+                String out = tc.getString(in);
+                strb.delete(start, start+len);
+                strb.insert(start, out);
+                length = out.length();
             } catch (Exception e) {
                 getLogger().error("TranscodingTransformer characters", e);
             }
         }
         this.contentHandler.characters(strb.toString().toCharArray(), start, length);
     }
-        
     
-    private String language = "eng";
+    private static String DEFAULT_LANG = "eng";
+    private String language;
     private TransCoder tc;
     private Stack elements;
-
+    private Stack languages;
+    private static String namespace = "http://stoa.org/2002/transcoder";
+    private static String tcName = "transcode";
+    
 }
