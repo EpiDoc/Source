8c1603ba17d47a2309566ab7e4d10fc8332e8039
agiacometti <agiacometti@85be4e97-ed4a-0410-b6a7-8d1dfdb05c0b>
2013-05-16 15:09:48 +0000

    font properties importing
    git-svn-id: svn+ssh://svn.code.sf.net/p/epidoc/code/trunk@1918 85be4e97-ed4a-0410-b6a7-8d1dfdb05c0b

--- transcoder/src/main/java/edu/unc/epidoc/transcoder/AbstractGreekConverter.java
+++ transcoder/src/main/java/edu/unc/epidoc/transcoder/AbstractGreekConverter.java
@@ -5,16 +5,32 @@
  */
 
 package edu.unc.epidoc.transcoder;
+import java.util.Properties;
 
 /**
  *
  * @author  hcayless
  */
 public abstract class AbstractGreekConverter extends AbstractConverter {
-    
+
     /** Creates a new instance of AbstractGreekConverter */
     public AbstractGreekConverter() {
         languages = new String[] {"grc", "gr", "greek", "la-Grek"};
+
+        Properties font_props = new Properties();
+
+        // This should find the prefix,
+        // for example if the parser is HellasParser, prefix will equal Hellas
+        Class c = this.getClass();
+        String prefix = c.getSimpleName().replaceFirst("Converter$", "");
+
+        try {
+            font_props.load(c.getResourceAsStream(prefix + "Fonts.properties"));
+            fonts = font_props.getProperty("Fonts").split(",");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace(System.out);
+        }
     }
-    
+
 }
--- transcoder/src/main/java/edu/unc/epidoc/transcoder/AbstractGreekParser.java
+++ transcoder/src/main/java/edu/unc/epidoc/transcoder/AbstractGreekParser.java
@@ -5,16 +5,32 @@
  */
 
 package edu.unc.epidoc.transcoder;
+import java.util.Properties;
 
 /**
  *
  * @author  hcayless
  */
 public abstract class AbstractGreekParser extends AbstractParser {
-    
+
     /** Creates a new instance of AbstractGreekParser */
     public AbstractGreekParser() {
         languages = new String[] {"grc", "gr", "greek", "la-Grek"};
+
+
+        Properties font_props = new Properties();
+
+        // This should find the prefix,
+        // for example if the parser is HellasParser, prefix will equal Hellas
+        Class c = this.getClass();
+        String prefix = c.getSimpleName().replaceFirst("Parser$", "");
+
+        try {
+            font_props.load(c.getResourceAsStream(prefix + "Fonts.properties"));
+            fonts = font_props.getProperty("Fonts").split(",");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace(System.out);
+        }
     }
-    
 }
